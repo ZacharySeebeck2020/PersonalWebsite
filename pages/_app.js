2e5dfcbf2df7035c0f8c4c832db126f684b4ec29
Zachary <seebeckzachary@gmail.com>
2022-07-12 14:13:05 -0400

    Finish building out a basic looking portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -7,37 +7,24 @@ import { faDiagramProject, faFile, faIdCard, faUser } from '@fortawesome/free-so
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className='container mx-auto'>
-      <nav className='w-full text-primary flex items-center h-10 pt-5'>
-        <ul className='flex'>
-          <li className='navLink'><a href="#about">About</a></li>
-          <li className='navLink'><a href="#experience">Experience</a></li>
-          <li className='navLink'><a href="#projects">Projects</a></li>
-          <li className='navLink'><a href="#contact">Contact</a></li>
-        </ul>
-        <ul className='flex ml-auto'>
-          <li className='navLink'><FontAwesomeIcon icon={faGithub}/> </li>
-          <li className='navLink'><FontAwesomeIcon icon={faTwitter}/> </li>
-        </ul>
-      </nav>
+	return (
+		<div className='container mx-auto px-10 mb-16'>
+			<nav className='max-w-[800px] mx-auto text-primary flex items-center h-10 pt-12'>
+				<ul className='flex -ml-5'>
+					<li className='navLink'><a href="#about">About</a></li>
+					<li className='navLink'><a href='#experience'>Experience</a></li>
+					<li className='navLink'><a href="#projects">Projects</a></li>
+					<li className='navLink'><a href="#contact">Contact</a></li>
+				</ul>
+				<ul className='flex ml-auto -mr-5'>
+					<li className='navLink'><FontAwesomeIcon icon={faGithub} /> </li>
+					<li className='navLink'><FontAwesomeIcon icon={faTwitter} /> </li>
+				</ul>
+			</nav>
 
-      <div>
-        <Component {...pageProps} />
-      </div>
-    </div>
-
-  )
-  // <Component {...pageProps} />
-}
-
-function HeaderLink({ icon, link, title }) {
-  return (
-    <div className='w-20 h-20 rounded-lg cursor-pointer transition-all duration-300 ease-in-out font-medium mx-2.5 text-sm text-white justify-center flex flex-col items-center bg-gradient-to-tl from-[#FA5252] to-[#DD2476] linked'>
-      <FontAwesomeIcon icon={icon} className="mb-3" />
-      {title}
-    </div>
-  )
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,128 @@
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faVoicemail } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Head from 'next/head'
+import Masonry from 'react-masonry-css'
+import ExperienceCard from '../components/ExperienceCard'
+import ProjectCard from '../components/ProjectCard'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Zachary Seebeck - Software Engineer</title>
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
-        </h1>
+			<div className='mt-16 max-w-[800px] mx-auto'>
+				<div id="about" className={styles.hero}>
+					<div className={styles.heroText}>
+						<h1 className={styles.heroTitle}>Zachary Seebeck</h1>
+						<h2 className={styles.heroSubtitle}>Software Engineer</h2>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+						<p className={styles.heroDescription}>
+							Building software is my passion. I'm a full-stack developer with a background in
+							PHP and Next.JS who wants to help the world build better solutions to their problems.
+						</p>
+					</div>
+					<div className={styles.heroImg}>
+						<img src="/img/me_face.jpg" alt="Headshot Image" className={styles.heroHeadshot} />
+					</div>
+				</div>
+			</div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+			<div id="experience" className='mt-16 max-w-[800px] mx-auto'>
+				<h1 className='header'>Experience</h1>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+				<ExperienceCard
+					name={'Newtech Dealer Services'}
+					date={'Jan 2019 - May 2021'}
+					content={`
+						Worked on software for dealerships to easily manage vehicle inventory 
+						and handle customer purchase flows. Main software worked on was Inventory 
+						Fusion and Process Pro Online. 
+					`}
+				/>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+				<ExperienceCard
+					name={'Vascular Health Clinics'}
+					date={'May 2021 - Sept 2021'}
+					content={`
+						Created internal tools for managing patient data as well as software that 
+						interfaced with patient monitors to monitor patients remotely.
+					`}
+				/>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+				<ExperienceCard
+					name={'Full Circle Media'}
+					date={'Sept 2021 - Present'}
+					content={`
+						Worked with a variety of clients to build online experiences for customers
+						and B2B e-commerce stores. Also worked on building custom purchase order 
+						management software that interfaces with the BigCommerce e-commerce platform.
+						
+						Also worked on managing backend services and integrations with 3rd parties,
+						and setting up a custom Docker CI/CD pipeline to host specialized applications.
+						 
+					`}
+				/>
+			</div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			<div id="projects" className='mt-16 max-w-[800px] mx-auto'>
+				<h1 className='header'>Projects</h1>
+
+				<ProjectCard
+					title={'Azera Design'}
+					description={'My web development and design studio for local businesses in the Bay City, MI area.'}
+					href="https://azera.xyz"
+					color1={'#ff1178'}
+					color2={'#11ff98'}
+				/>
+				<Masonry breakpointCols={3} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+					<ProjectCard
+						title={'PomoTask'}
+						description={'A task management app for the Pomodoro technique written in Next.JS.'}
+						href="https://github.com/ZacharySeebeck2020/PomoTask"
+						color1={'#FE0000'}
+						color2={'#00FEFE'}
+					/>
+					<ProjectCard
+						title={'OctoShort'}
+						description={`
+							A link shortener built in Next.JS that allows unauthenticated link shortening,
+							as well as link tracking for business' to see what kinds of devices are using 
+							the short links.
+						`}
+						href="https://github.com/ZacharySeebeck2020/OctoShort"
+						color1={'#FFF205'}
+						color2={'#0512FF'}
+					/>
+					<ProjectCard
+						title={'Road Visualizer'}
+						description={`
+							A road visualizer with the ability to lay out city streets, set speed 
+							limits, and place stop lights with custom timing. Written in C++.
+						`}
+						color1={'#7cff01'}
+						color2={'#8401FF'}
+					/>
+				</Masonry>
+			</div>
+
+			<div id="contact" className='mt-16 max-w-[800px] mx-auto'>
+				<h1 className='header'>Get In Touch</h1>
+				<div className='flex justify-center items-center text-white gap-5'>
+					<a href='https://github.com/ZacharySeebeck2020' className='rounded bg-gray-800 py-2 px-3'>
+						<FontAwesomeIcon icon={faGithub}/>
+					</a>
+					<a href='mailto:seebeckzachary@gmail.com' className='rounded bg-gray-800 py-2 px-3'>
+						<FontAwesomeIcon icon={faEnvelope}/>
+					</a>
+					<a href='https:/twitter.com/Xeinix' className='rounded bg-gray-800 py-2 px-3'>
+						<FontAwesomeIcon icon={faTwitter}/>
+					</a>
+				</div>
+			</div>
+		</>
+	)
 }
