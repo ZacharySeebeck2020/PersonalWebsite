e9f9d6d534c9a38bfbbb0aa685ff9a930f7b0299
Zachary Seebeck <seebeckzachary@gmail.com>
2022-07-30 21:59:46 -0400

    Maybe push to gh pages?

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   output: 'standalone',
 }
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/zacharyseebeck2020.github.io/' : ''
+}
